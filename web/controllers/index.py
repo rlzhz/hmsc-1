29d1c2a5a7e67f3d8e9a28c170fa33b097b32c79
Bruce <1046244623@qq.com>
2020-04-24 16:40:12 +0800

    index渲染

--- web/controllers/index.py
+++ web/controllers/index.py
@@ -1,9 +1,10 @@
 from application import app
 from flask import Blueprint,render_template
+from common.libs.Helper import ops_render
 
 route_index = Blueprint("index_page",__name__)
 
 @route_index.route('/')
 def index():
-    return render_template("index/index.html")
+    return ops_render("index/index.html",{})
    
--- web/controllers/user/User.py
+++ web/controllers/user/User.py
@@ -1,4 +1,6 @@
-from flask import Blueprint,render_template,request,jsonify,make_response,redirect,g
+from flask import Blueprint,request,jsonify,make_response,redirect,g
+
+from application import app
 from common.models.User import User
 from common.libs.user.UserService import UserService
 from common.libs.UrlManager import UrlManager
@@ -54,7 +56,7 @@ def login():
     
     response = make_response(json.dumps({'code':200,'msg':'登录成功~~~'}))
     # Cookie中存入的信息是user_info.uid,user_info
-    response.set_cookie("hmsc_1901C","%s@%s"%(UserService.generateAuthCode(user_info),user_info.uid),60*60*24*15)
+    response.set_cookie(app.config['AUTH_COOKIE_NAME'],"%s@%s"%(UserService.generateAuthCode(user_info),user_info.uid),60*60*24*15)
     return response
     
 
